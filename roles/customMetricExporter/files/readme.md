0dd1d18792a7f6741005f659a243d405ed286429
Davinder Pal <dpsangwal@gmail.com>
2022-09-13 01:38:02 +0300

    * added support to use SSL for Metric Scraping * added initial support MTLS for fresh cluster

--- roles/customMetricExporter/files/readme.md
+++ roles/customMetricExporter/files/readme.md
@@ -1,7 +1,7 @@
 # Zooki
 Apache Zookeeper Metric Collector
 
-Usage: `python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/ dev-env-zookeeper`
+Usage: `python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/ dev-env-zookeeper true`
 
 
-        <python version> <script path> <Zookeeper Partition> <Metric Output Folder> <cluster environment name>
+        <python version> <script path> <Zookeeper Partition> <Metric Output Folder> <cluster environment name> <zookeeper-ssl-status>
--- roles/customMetricExporter/files/zooki.py
+++ roles/customMetricExporter/files/zooki.py
@@ -1,11 +1,11 @@
 # Cloned from: https://github.com/116davinder/zooki
 
-# usage: python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/ dev-zookeeper
+# usage: python3 zooki.py /zookeeper /zookeeper/zookeeper-logs/ dev-zookeeper false
 
 # This script suppose to export all zookeeper metric from one node and write to file
 # from where splunk/cloudwatch like tools can read it.
 
-from urllib import request
+import requests
 import shutil
 from socket import gethostname
 import json
@@ -14,11 +14,16 @@ import os.path
 from datetime import datetime
 
 
-class zooki:
+class Zooki:
     def __init__(self):
         self.zAddr = gethostname()
         self.zPort = 8080
-        self.zHttpAddr = "http://" + self.zAddr + ":" + str(self.zPort) + "/commands/"
+        try:
+            self.zHttpScheme = "https://" if sys.argv[4].lower() == "true" else "http://"
+        except IndexError:
+            self.zHttpScheme = "http://"
+
+        self.zHttpAddr = f"{self.zHttpScheme}{self.zAddr}:{self.zPort}/commands/"
         self.cTimeNow = str(datetime.now())
 
     def getStorageMetric(self):
@@ -34,9 +39,9 @@ class zooki:
         return json.dumps(_sMetric)
 
     def getZMetric(self, commandPath):
-        with request.urlopen(self.zHttpAddr + commandPath) as f:
-            if f.status == 200:
-                _zMetric = json.loads(f.read().decode("utf-8"))
+        with requests.get(self.zHttpAddr + commandPath, verify=False) as f:
+            if f.status_code == 200:
+                _zMetric = f.json()
                 _zMetric["@timestamp"] = self.cTimeNow
                 _zMetric["environment"] = sys.argv[3]
             else:
@@ -46,9 +51,9 @@ class zooki:
     # json retruned by monitor is too big to be handled by splunk indexer
     # so separate function to reduce the json size
     def getMonitorMetric(self):
-        with request.urlopen(self.zHttpAddr + "monitor") as f:
-            if f.status == 200:
-                _MM = json.loads(f.read().decode("utf-8"))
+        with requests.get(self.zHttpAddr + "monitor", verify=False) as f:
+            if f.status_code == 200:
+                _MM = f.json()
                 _zMetric = {
                     "@timestamp": self.cTimeNow,
                     "environment": sys.argv[3],
@@ -75,7 +80,7 @@ def main():
 
     commandPaths = ["connections", "leader", "watch_summary"]
 
-    z = zooki()
+    z = Zooki()
     with open(os.path.join(sys.argv[2], "disk.out"), "w") as zMetricFile:
         zMetricFile.write(z.getStorageMetric())
 
